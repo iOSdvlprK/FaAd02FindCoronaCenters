f2b245bb44ad2f35cd0366088a64c3f4683c339b
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2024-06-26 23:29:34 +0900

    Initial Commit

---
+++ FaAd02FindCoronaCenters/FaAd02FindCoronaCentersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaAd02FindCoronaCentersApp.swift
+//  FaAd02FindCoronaCenters
+//
+//  Created by joe on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct FaAd02FindCoronaCentersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
